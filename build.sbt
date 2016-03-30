283d1031b0970fdb97d36f756d4c9bca71d083e0
Jose Rodriguez <jose@meetup.com>
2016-03-29 22:25:09 -0400

    Added initial testing.

--- build.sbt
+++ build.sbt
@@ -12,3 +12,13 @@ sbtPlugin := true
 addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.6.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.0.6")
+
+lazy val root = (project in file("."))
+  .configs(IntegrationTest)
+
+Defaults.itSettings
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "2.2.6" % "it",
+  "org.scalacheck" %% "scalacheck" % "1.11.5" % "it"
+)
---
+++ src/it/scala/com/meetup/blt/sbt/DockerPackagePluginTest.scala
@@ -0,0 +1,24 @@
+package com.meetup.blt.sbt
+
+import java.io.File
+
+import org.scalatest.{Matchers, FunSpec}
+
+/**
+  * Created by jose on 3/29/16.
+  */
+class DockerPackagePluginTest extends FunSpec with Matchers {
+
+  val dockerPackageDir = new File("src/it/sbt/docker-package")
+
+  val sbt = new Sbt(dockerPackageDir)
+
+  it("should load image name from Makefile") {
+    sbt.secondToLast("show docker:dockerTarget") should include ("mup.cr/blt/publish-test")
+  }
+
+  it("should load base tag from Makefile") {
+    sbt.lastLine("export dockerBaseImage") shouldBe "java:8"
+  }
+
+}
