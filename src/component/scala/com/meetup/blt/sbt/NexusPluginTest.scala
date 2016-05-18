95547c07af5c173131501b0ba8f1f66257eee5a7
Jose Rodriguez <jose@meetup.com>
2016-05-18 14:05:01 -0400

    Move it tests to component tests.

--- src/component/scala/com/meetup/blt/sbt/NexusPluginTest.scala
+++ src/component/scala/com/meetup/blt/sbt/NexusPluginTest.scala
@@ -6,7 +6,7 @@ import org.scalatest.{Matchers, FunSpec}
 
 class NexusPluginTest extends FunSpec with Matchers {
 
-  val nexusDir = new File("src/it/sbt/nexus")
+  val nexusDir = new File("src/component/sbt/nexus")
 
   val sbt = new Sbt(nexusDir)
 
